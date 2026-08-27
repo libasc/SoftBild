6abdfcb7b2daeb99a630835ed708b3a734699be1
cieos <zafar75190@gmail.com>
2026-08-28 04:43:58 +0530

    Updated page scrolling behaviour

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/home/Home'
 import About from '../pages/about/About'
 import Services from '../pages/services/Services'
@@ -11,30 +11,34 @@ import Contactus from '../pages/contact/Contactus'
 import BlogDetails from '../pages/blog/BlogDetails'
 import Portfolio from '../pages/portfolio/Portfolio'
 import PortfolioDetail from '../pages/portfolio/PortfolioDetail'
+import ScrollToTop from '../components/ScrollToTop'
 
 function Routing() {
   return (
     <>
     <section className='main'>
-      <Routes>
-        <Route path='/' element={<AppLayout />}>
-            <Route path='/' element={<Home></Home>}></Route>
-            <Route path='/Aboutus' element={<About></About>}></Route>
-            <Route path='/Services' element={<Services></Services>}></Route>
-            <Route path='/OnDemand-Resourse' element={<OnDemandResource></OnDemandResource>}></Route>
-            <Route path='/Blog' element={<Blog></Blog>}></Route>
-            <Route path='/Portfolio' element={<Portfolio></Portfolio>}></Route>
-            <Route path='/Portfolio/:slug' element={<PortfolioDetail />}/>
-            <Route path='/Pages' element={<Page></Page>}></Route>
-            <Route path='/Contact' element={<Contactus></Contactus>}></Route>
+      {/* <BrowserRouter> */}
+        <ScrollToTop />
+          <Routes>
+            <Route path='/' element={<AppLayout />}>
+                <Route path='/' element={<Home></Home>}></Route>
+                <Route path='/Aboutus' element={<About></About>}></Route>
+                <Route path='/Services' element={<Services></Services>}></Route>
+                <Route path='/OnDemand-Resourse' element={<OnDemandResource></OnDemandResource>}></Route>
+                <Route path='/Blog' element={<Blog></Blog>}></Route>
+                <Route path='/Portfolio' element={<Portfolio></Portfolio>}></Route>
+                <Route path='/Portfolio/:slug' element={<PortfolioDetail />}/>
+                <Route path='/Pages' element={<Page></Page>}></Route>
+                <Route path='/Contact' element={<Contactus></Contactus>}></Route>
 
-            <Route path='/BlogDetails/:id' element={<BlogDetails></BlogDetails>}></Route>
-            <Route path='/EditBlog/:id' element={<Page></Page>}></Route>
-            <Route path='/DeleteBlog/:id' element={<Page></Page>}></Route>
-        </Route>
+                <Route path='/BlogDetails/:id' element={<BlogDetails></BlogDetails>}></Route>
+                <Route path='/EditBlog/:id' element={<Page></Page>}></Route>
+                <Route path='/DeleteBlog/:id' element={<Page></Page>}></Route>
+            </Route>
 
-        <Route path='*' element={<h1>Sorry!, Page not found</h1>}></Route>
-      </Routes>
+            <Route path='*' element={<h1>Sorry!, Page not found</h1>}></Route>
+          </Routes>
+      {/* </BrowserRouter> */}
     </section>
 
     {/* <Footer></Footer> */}
